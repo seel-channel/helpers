a9b141c7aedf2b496248748a53e186ceacca4f85
Luis Felipe Murguia Ramos <felipelfmr1@gmail.com>
2021-10-18 15:49:58 -0500

    [edited] We added some PrintColorStyle to print functions

--- lib/helpers/print.dart
+++ lib/helpers/print.dart
@@ -3,14 +3,6 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 
 class _AnsiColor {
-  static const ansiEscape = '\x1B[';
-  static const ansiDefault = '${ansiEscape}0m';
-
-  int? _bg;
-  late int _fg;
-
-  final PrintColorStyle style;
-
   _AnsiColor(this.style) {
     _fg = _fromRgbColorTo8bitColor(style.foreground);
     if (style.background != null) {
@@ -18,12 +10,13 @@ class _AnsiColor {
     }
   }
 
-  int _fromRgbColorTo8bitColor(Color color) {
-    return ((color.red / 255) * 5).floor() * 36 +
-        ((color.green / 255) * 5).floor() * 6 +
-        ((color.blue / 255) * 5).floor() +
-        16;
-  }
+  static const ansiDefault = '${ansiEscape}0m';
+  static const ansiEscape = '\x1B[';
+
+  final PrintColorStyle style;
+
+  int? _bg;
+  late int _fg;
 
   @override
   String toString() {
@@ -38,72 +31,381 @@ class _AnsiColor {
     return sb.toString();
   }
 
-  String call(Object? msg) => '${this}$msg$ansiDefault';
+  int _fromRgbColorTo8bitColor(Color color) {
+    return ((color.red / 255) * 5).floor() * 36 +
+        ((color.green / 255) * 5).floor() * 6 +
+        ((color.blue / 255) * 5).floor() +
+        16;
+  }
+
+  String message(Object? msg) => '${this}$msg$ansiDefault';
 }
 
 void printColor(Object? object, PrintColorStyle style) {
-  final value = _AnsiColor(style)(object);
+  final value = _AnsiColor(style).message(object);
   if (style.prefix.isEmpty) {
     debugPrint(value);
   } else {
-    log(value, name: style.prefix);
+    log(value, name: style.prefix.isEmpty ? "LOG" : style.prefix);
   }
 }
 
 class PrintColorStyle {
-  final bool bold, underline, italic;
-  final String prefix;
-  final Color foreground;
-  final Color? background;
-
   const PrintColorStyle({
-    required this.foreground,
     this.background,
     this.bold = false,
+    this.foreground = Colors.blue,
     this.italic = false,
-    this.underline = false,
     this.prefix = "",
+    this.underline = false,
   });
+
+  final Color? background;
+  final bool bold;
+  final Color foreground;
+  final bool italic;
+  final String prefix;
+  final bool underline;
 }
 
-void printWhite(Object? object, {bool bold = false}) =>
-    printColor(object, PrintColorStyle(foreground: Colors.white, bold: bold));
-void printBlack(Object? object, {bool bold = false}) =>
-    printColor(object, PrintColorStyle(foreground: Colors.black, bold: bold));
-void printBrown(Object? object, {bool bold = false}) =>
-    printColor(object, PrintColorStyle(foreground: Colors.brown, bold: bold));
+void printWhite(
+  Object? object, {
+  bool bold = false,
+  bool italic = false,
+  bool underline = false,
+  String prefix = "",
+}) =>
+    printColor(
+      object,
+      PrintColorStyle(
+        foreground: Colors.white,
+        bold: bold,
+        prefix: prefix,
+        underline: underline,
+        italic: italic,
+      ),
+    );
+void printBlack(
+  Object? object, {
+  bool bold = false,
+  bool italic = false,
+  bool underline = false,
+  String prefix = "",
+}) =>
+    printColor(
+      object,
+      PrintColorStyle(
+        foreground: Colors.black,
+        bold: bold,
+        prefix: prefix,
+        underline: underline,
+        italic: italic,
+      ),
+    );
+void printBrown(
+  Object? object, {
+  bool bold = false,
+  bool italic = false,
+  bool underline = false,
+  String prefix = "",
+}) =>
+    printColor(
+      object,
+      PrintColorStyle(
+        foreground: Colors.brown,
+        bold: bold,
+        prefix: prefix,
+        underline: underline,
+        italic: italic,
+      ),
+    );
 
-void printRed(Object? object, {bool bold = false}) => printColor(
-    object, PrintColorStyle(foreground: Colors.redAccent, bold: bold));
-void printBlue(Object? object, {bool bold = false}) => printColor(
-    object, PrintColorStyle(foreground: Colors.blueAccent, bold: bold));
-void printPink(Object? object, {bool bold = false}) => printColor(
-    object, PrintColorStyle(foreground: Colors.pinkAccent, bold: bold));
-void printLime(Object? object, {bool bold = false}) => printColor(
-    object, PrintColorStyle(foreground: Colors.limeAccent, bold: bold));
-void printTeal(Object? object, {bool bold = false}) => printColor(
-    object, PrintColorStyle(foreground: Colors.tealAccent, bold: bold));
-void printCyan(Object? object, {bool bold = false}) => printColor(
-    object, PrintColorStyle(foreground: Colors.cyanAccent, bold: bold));
-void printGreen(Object? object, {bool bold = false}) => printColor(
-    object, PrintColorStyle(foreground: Colors.greenAccent, bold: bold));
-void printAmber(Object? object, {bool bold = false}) => printColor(
-    object, PrintColorStyle(foreground: Colors.amberAccent, bold: bold));
-void printOrange(Object? object, {bool bold = false}) => printColor(
-    object, PrintColorStyle(foreground: Colors.orangeAccent, bold: bold));
-void printYellow(Object? object, {bool bold = false}) => printColor(
-    object, PrintColorStyle(foreground: Colors.yellowAccent, bold: bold));
-void printIndigo(Object? object, {bool bold = false}) => printColor(
-    object, PrintColorStyle(foreground: Colors.indigoAccent, bold: bold));
-void printPurple(Object? object, {bool bold = false}) => printColor(
-    object, PrintColorStyle(foreground: Colors.purpleAccent, bold: bold));
-void printBlueGrey(Object? object, {bool bold = false}) => printColor(
-    object, PrintColorStyle(foreground: Colors.blueGrey, bold: bold));
-void printDeepPurple(Object? object, {bool bold = false}) => printColor(
-    object, PrintColorStyle(foreground: Colors.deepPurpleAccent, bold: bold));
-void printLightGreen(Object? object, {bool bold = false}) => printColor(
-    object, PrintColorStyle(foreground: Colors.lightGreenAccent, bold: bold));
-void printLightBlue(Object? object, {bool bold = false}) => printColor(
-    object, PrintColorStyle(foreground: Colors.lightBlueAccent, bold: bold));
-void printDeepOrange(Object? object, {bool bold = false}) => printColor(
-    object, PrintColorStyle(foreground: Colors.deepOrangeAccent, bold: bold));
+void printRed(
+  Object? object, {
+  bool bold = false,
+  bool italic = false,
+  bool underline = false,
+  String prefix = "",
+}) =>
+    printColor(
+      object,
+      PrintColorStyle(
+        foreground: Colors.redAccent,
+        bold: bold,
+        prefix: prefix,
+        underline: underline,
+        italic: italic,
+      ),
+    );
+void printBlue(
+  Object? object, {
+  bool bold = false,
+  bool italic = false,
+  bool underline = false,
+  String prefix = "",
+}) =>
+    printColor(
+      object,
+      PrintColorStyle(
+        foreground: Colors.blueAccent,
+        bold: bold,
+        prefix: prefix,
+        underline: underline,
+        italic: italic,
+      ),
+    );
+void printPink(
+  Object? object, {
+  bool bold = false,
+  bool italic = false,
+  bool underline = false,
+  String prefix = "",
+}) =>
+    printColor(
+      object,
+      PrintColorStyle(
+        foreground: Colors.pinkAccent,
+        bold: bold,
+        prefix: prefix,
+        underline: underline,
+        italic: italic,
+      ),
+    );
+void printLime(
+  Object? object, {
+  bool bold = false,
+  bool italic = false,
+  bool underline = false,
+  String prefix = "",
+}) =>
+    printColor(
+      object,
+      PrintColorStyle(
+        foreground: Colors.limeAccent,
+        bold: bold,
+        prefix: prefix,
+        underline: underline,
+        italic: italic,
+      ),
+    );
+void printTeal(
+  Object? object, {
+  bool bold = false,
+  bool italic = false,
+  bool underline = false,
+  String prefix = "",
+}) =>
+    printColor(
+      object,
+      PrintColorStyle(
+        foreground: Colors.tealAccent,
+        bold: bold,
+        prefix: prefix,
+        underline: underline,
+        italic: italic,
+      ),
+    );
+void printCyan(
+  Object? object, {
+  bool bold = false,
+  bool italic = false,
+  bool underline = false,
+  String prefix = "",
+}) =>
+    printColor(
+      object,
+      PrintColorStyle(
+        foreground: Colors.cyanAccent,
+        bold: bold,
+        prefix: prefix,
+        underline: underline,
+        italic: italic,
+      ),
+    );
+void printGreen(
+  Object? object, {
+  bool bold = false,
+  bool italic = false,
+  bool underline = false,
+  String prefix = "",
+}) =>
+    printColor(
+      object,
+      PrintColorStyle(
+        foreground: Colors.greenAccent,
+        bold: bold,
+        prefix: prefix,
+        underline: underline,
+        italic: italic,
+      ),
+    );
+void printAmber(
+  Object? object, {
+  bool bold = false,
+  bool italic = false,
+  bool underline = false,
+  String prefix = "",
+}) =>
+    printColor(
+      object,
+      PrintColorStyle(
+        foreground: Colors.amberAccent,
+        bold: bold,
+        prefix: prefix,
+        underline: underline,
+        italic: italic,
+      ),
+    );
+void printOrange(
+  Object? object, {
+  bool bold = false,
+  bool italic = false,
+  bool underline = false,
+  String prefix = "",
+}) =>
+    printColor(
+      object,
+      PrintColorStyle(
+        foreground: Colors.orangeAccent,
+        bold: bold,
+        prefix: prefix,
+        underline: underline,
+        italic: italic,
+      ),
+    );
+void printYellow(
+  Object? object, {
+  bool bold = false,
+  bool italic = false,
+  bool underline = false,
+  String prefix = "",
+}) =>
+    printColor(
+      object,
+      PrintColorStyle(
+        foreground: Colors.yellowAccent,
+        bold: bold,
+        prefix: prefix,
+        underline: underline,
+        italic: italic,
+      ),
+    );
+void printIndigo(
+  Object? object, {
+  bool bold = false,
+  bool italic = false,
+  bool underline = false,
+  String prefix = "",
+}) =>
+    printColor(
+      object,
+      PrintColorStyle(
+        foreground: Colors.indigoAccent,
+        bold: bold,
+        prefix: prefix,
+        underline: underline,
+        italic: italic,
+      ),
+    );
+void printPurple(
+  Object? object, {
+  bool bold = false,
+  bool italic = false,
+  bool underline = false,
+  String prefix = "",
+}) =>
+    printColor(
+      object,
+      PrintColorStyle(
+        foreground: Colors.purpleAccent,
+        bold: bold,
+        prefix: prefix,
+        underline: underline,
+        italic: italic,
+      ),
+    );
+void printBlueGrey(
+  Object? object, {
+  bool bold = false,
+  bool italic = false,
+  bool underline = false,
+  String prefix = "",
+}) =>
+    printColor(
+      object,
+      PrintColorStyle(
+        foreground: Colors.blueGrey,
+        bold: bold,
+        prefix: prefix,
+        underline: underline,
+        italic: italic,
+      ),
+    );
+void printDeepPurple(
+  Object? object, {
+  bool bold = false,
+  bool italic = false,
+  bool underline = false,
+  String prefix = "",
+}) =>
+    printColor(
+      object,
+      PrintColorStyle(
+        foreground: Colors.deepPurpleAccent,
+        bold: bold,
+        prefix: prefix,
+        underline: underline,
+        italic: italic,
+      ),
+    );
+void printLightGreen(
+  Object? object, {
+  bool bold = false,
+  bool italic = false,
+  bool underline = false,
+  String prefix = "",
+}) =>
+    printColor(
+      object,
+      PrintColorStyle(
+        foreground: Colors.lightGreenAccent,
+        bold: bold,
+        prefix: prefix,
+        underline: underline,
+        italic: italic,
+      ),
+    );
+void printLightBlue(
+  Object? object, {
+  bool bold = false,
+  bool italic = false,
+  bool underline = false,
+  String prefix = "",
+}) =>
+    printColor(
+      object,
+      PrintColorStyle(
+        foreground: Colors.lightBlueAccent,
+        bold: bold,
+        prefix: prefix,
+        underline: underline,
+        italic: italic,
+      ),
+    );
+void printDeepOrange(
+  Object? object, {
+  bool bold = false,
+  bool italic = false,
+  bool underline = false,
+  String prefix = "",
+}) =>
+    printColor(
+      object,
+      PrintColorStyle(
+        foreground: Colors.deepOrangeAccent,
+        bold: bold,
+        prefix: prefix,
+        underline: underline,
+        italic: italic,
+      ),
+    );
