1ccf0d9c1bd362d404b301927787f1709638836d
Luis Felipe Murguia Ramos <felipelfmr1@gmail.com>
2022-01-08 16:22:45 -0600

    [widgets] We created a builder to notify if the keyboard is visible

---
+++ lib/helpers/widgets/keyboard.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class KeyboardVisibilityBuilder extends StatefulWidget {
+  const KeyboardVisibilityBuilder({
+    Key? key,
+    this.child,
+    required this.builder,
+  }) : super(key: key);
+
+  final Widget Function(
+    BuildContext context,
+    bool isKeyboardVisible,
+    Widget? child,
+  ) builder;
+
+  final Widget? child;
+
+  @override
+  _KeyboardVisibilityBuilderState createState() =>
+      _KeyboardVisibilityBuilderState();
+}
+
+class _KeyboardVisibilityBuilderState extends State<KeyboardVisibilityBuilder>
+    with WidgetsBindingObserver {
+  final WidgetsBinding _instance = WidgetsBinding.instance!;
+  bool _isKeyboardVisible = false;
+
+  @override
+  void didChangeMetrics() {
+    final bottomInset = _instance.window.viewInsets.bottom;
+    final newValue = bottomInset > 0.0;
+    if (newValue != _isKeyboardVisible) {
+      _isKeyboardVisible = newValue;
+      if (mounted) setState(() {});
+    }
+    super.didChangeMetrics();
+  }
+
+  @override
+  void dispose() {
+    _instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    _instance.addObserver(this);
+    didChangeMetrics();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) => widget.builder(
+        context,
+        _isKeyboardVisible,
+        widget.child,
+      );
+}
+
+class DismissKeyboard extends StatelessWidget {
+  ///Tapping on a Widget will apply the FocusScope to it and hide the keyboard.
+  const DismissKeyboard({
+    Key? key,
+    this.child,
+    this.behavior = HitTestBehavior.opaque,
+  }) : super(key: key);
+
+  final HitTestBehavior behavior;
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        final FocusScopeNode focus = FocusScope.of(context);
+        if (!focus.hasPrimaryFocus) focus.requestFocus(FocusNode());
+      },
+      behavior: behavior,
+      child: child,
+    );
+  }
+}
