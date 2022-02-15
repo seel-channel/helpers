e6da69032edc728c1561fbf2a7dff342321622f2
Luis Felipe Murguia Ramos <felipelfmr1@gmail.com>
2022-02-15 12:07:58 -0600

    [color extension] We added to get complementary color, and convert color to HSL and HSV

--- lib/helpers/extensions/color.dart
+++ lib/helpers/extensions/color.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
-extension HexColorExtension on Color {
+extension HSLColorExtension on HSLColor {
+  HSLColor withRelativeLightness(double lightness) {
+    return withLightness(this.lightness * lightness);
+  }
+}
+
+extension ColorExtension on Color {
   /// String is in the format "RRGGBB" or "AARRGGBB" with an optional hash sing "#".
   static Color fromHex(String hex) {
     final buffer = StringBuffer();
@@ -9,13 +15,37 @@ extension HexColorExtension on Color {
     return Color(int.parse(buffer.toString(), radix: 16));
   }
 
+  /// Returns a new color that matches this color with the alpha channel
+  /// replaced with the given `opacity` (which ranges from 0.0 to 1.0).
+  ///
+  /// Out of range values will have unexpected effects.
+  Color withRelativeOpacity(double opacity) {
+    return withOpacity(this.opacity * opacity);
+  }
+
+  Color toComplementary() {
+    final HSLColor color = toHSL();
+    double hue = color.hue + 180;
+    if (hue > 360) hue -= 360;
+    return HSLColor.fromAHSL(
+      color.alpha,
+      hue,
+      color.saturation,
+      color.lightness,
+    ).toColor();
+  }
+
+  HSLColor toHSL() => HSLColor.fromColor(this);
+
+  HSVColor toHSV() => HSVColor.fromColor(this);
+
   /// **hashSing:**
-  ///  - True = #AARRGGBB;
-  ///  - False = AARRGGBB
+  ///  - true: #AARRGGBB;
+  ///  - false: AARRGGBB
   ///
   /// **alphaValue:**
-  ///  - True = #AARRGGBB;
-  ///  - False = #RRGGBB
+  ///  - true: #AARRGGBB;
+  ///  - false: #RRGGBB
   String toHex({bool hashSing = true, bool alphaValue = false}) =>
       '${hashSing ? '#' : ''}'
       '${alphaValue ? alpha.toRadixString(16).padLeft(2, '0') : ''}'
