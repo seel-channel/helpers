fc743fb667099e2266af250c357a4845d61883aa
Luis Felipe Murguia Ramos <felipelfmr1@gmail.com>
2022-07-10 16:24:29 -0500

    [MultipleListUpdater] we added replaceOrAdd

--- lib/helpers/lists/multiple_list_updater.dart
+++ lib/helpers/lists/multiple_list_updater.dart
@@ -15,17 +15,29 @@ class MultipleListUpdater<T> {
 
   List<List<T>> get elements => _elements ?? _builder!();
 
+  void replaceOrAdd(ReplaceValidator<T> validator, T value) {
+    if (!replaceWhere(validator, (e) => value)) add(value);
+  }
+
   void add(T value) {
     for (final item in elements) {
       item.add(value);
     }
   }
 
-  void replaceWhere(
-      ReplaceValidator<T> validator, ReplaceValue<T> replaceValue) {
-    for (final item in elements) {
-      item.replaceWhere(validator, replaceValue);
+  void clear() {
+    for (final element in elements) {
+      element.clear();
     }
+  }
+
+  bool replaceWhere(
+      ReplaceValidator<T> validator, ReplaceValue<T> replaceValue) {
+    bool hasReplaced = false;
+    for (final item in elements) {
+      if (item.replaceWhere(validator, replaceValue)) hasReplaced = true;
+    }
+    return hasReplaced;
   }
 
   void remove(T value) {
